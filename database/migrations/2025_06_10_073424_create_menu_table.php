013bb00b0de7c1c93a1642c05db62c9051937e49
Izath <izathbsl123@gmail.com>
2025-06-10 15:22:53 +0700

    kolaborasi project

---
+++ database/migrations/2025_06_10_073424_create_menu_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('menu', function (Blueprint $table) {
+            $table->id();
+            $table->string('menu_text');
+            $table->string('menu_icon')->nullable();
+            $table->string('menu_url');
+            $table->integer('menu_order')->default(0);
+            $table->boolean('status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('menu');
+    }
+};
