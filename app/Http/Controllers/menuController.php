41614a89d0f2ec1da3946c6621eff6e2021eb473
AnnasTasyaR <tasyaannas@gmail.com>
2025-06-10 16:05:24 +0700

    controller menu

--- app/Http/Controllers/menuController.php
+++ app/Http/Controllers/menuController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class menuController extends Controller
+class MenuController extends Controller
 {
     /**
      * Display a listing of the resource.
